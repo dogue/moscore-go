420625ece5db86324d59fdf9ae9df57046b1fd18
Dogue <dogue@oddhelix.com>
2024-03-02 22:40:44 -0500

    finish ADC tests

--- arithmetic_test.go
+++ arithmetic_test.go
@@ -56,3 +56,31 @@ func TestAddAbsolute(t *testing.T) {
 		t.Errorf("incorrect sum: wanted=%#02x, got=%#02x", 0x0a, core.acc)
 	}
 }
+
+func TestAddAbsoluteIndexed(t *testing.T) {
+	// X
+	bus := newBus([]uint8{0x7D, 0x40})
+	bus.Write(0x0042, 0x05)
+	core := New(&bus)
+	core.Reset()
+	core.acc = 0x05
+	core.idx = 0x02
+	core.Step()
+
+	if core.acc != 0x0a {
+		t.Errorf("incorrect sum: wanted=%#02x, got=%#02x", 0x0a, core.acc)
+	}
+
+	// Y
+	bus = newBus([]uint8{0x79, 0x40})
+	bus.Write(0x0042, 0x05)
+	core = New(&bus)
+	core.Reset()
+	core.acc = 0x05
+	core.idy = 0x02
+	core.Step()
+
+	if core.acc != 0x0a {
+		t.Errorf("incorrect sum: wanted=%#02x, got=%#02x", 0x0a, core.acc)
+	}
+}
