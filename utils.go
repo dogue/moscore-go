6cb84efc5dcf2873829f756db1285b943af0348c
Dogue <dogue@oddhelix.com>
2024-03-02 22:54:26 -0500

    comment typo fix

--- utils.go
+++ utils.go
@@ -1,11 +1,11 @@
 package moscore
 
-// (low byte, high byte) -> 16-bit address
+// low byte, high byte -> 16-bit address
 func addrFromBytes(low, high uint8) uint16 {
 	return (uint16(high) << 8) | uint16(low)
 }
 
-// 16-bit address -> (low byte, high byte)
+// 16-bit address -> low byte, high byte
 func bytesFromAddr(addr uint16) (uint8, uint8) {
 	low := uint8(addr)
 	high := uint8(addr >> 8)
@@ -13,7 +13,7 @@ func bytesFromAddr(addr uint16) (uint8, uint8) {
 }
 
 // add two uint8's with carry
-// ripped off from standard library code for doing with with 32 bit ints
+// ripped off from standard library code for doing this with 32 bit ints
 func add8(x, y, carryIn uint8) (sum, carryOut uint8) {
 	sum16 := uint16(x) + uint16(y) + uint16(carryIn)
 	sum = uint8(sum16)
